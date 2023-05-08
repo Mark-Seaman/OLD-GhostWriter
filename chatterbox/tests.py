a6e1f390e209e5ca0b525d5639d64ec550c4bf41
Mark Seaman <seaman@SeaMac>
2023-05-07 19:41:25 -0600

    Debug AI workflow

--- chatterbox/tests.py
+++ chatterbox/tests.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 from django.test import TestCase
 from requests import get
+from chatterbox.ai import do_gpt_task
 from chatterbox.files import recursive_files
-from chatterbox.pub_script import pub_path, pub_script_command
+from chatterbox.pub_script import extract_outline, markdown_to_outline, pub_path, pub_script_command
 
 from chatterbox.text import text_lines
 
@@ -18,13 +19,16 @@ class DjangoTest(TestCase):
     def assertLines(self, page, min, max):
         response = get(page)
         self.assertEqual(response.status_code, 200)
-        lines = len(text_lines(response.text))
-        self.assertRange(lines, min, max, label=f"Lines in {page}")
+        self.assertNumLines(self, response.text, min, max)
 
     def assertText(self, page, text):
         response = get(page)
         self.assertEqual(response.status_code, 200)
         self.assertIn(text, response.text)
+
+    def assertNumLines(self, text, min, max):
+        lines = len(text_lines(text))
+        self.assertRange(lines, min, max, label=f"Lines in {text}")
 
     def assertRange(self, num, min, max, label="Value"):
         error = f"{label} {num} is not in range (min {min} and max {max})"
@@ -40,7 +44,7 @@ class GhostTest(DjangoTest):
     def test_pub_files(self):
         directory = pub_path('GhostWriter')
         self.assertEqual(str(directory), '/Users/seaman/Hammer/Documents/Shrinking-World-Pubs/GhostWriter')
-        self.assertFiles(directory, 20, 21)
+        self.assertFiles(directory, 20, 40)
 
     def test_project(self):
         pub_script_command('project', ['GhostWriter','ghost'])
@@ -50,3 +54,21 @@ class GhostTest(DjangoTest):
         self.assertTrue(js.exists())
         ai = pub_path('GhostWriter')/'AI'
         self.assertTrue(ai.exists())
+
+    def test_outliner(self):
+        directory = pub_path('GhostWriter')
+        doc = directory/'Pub/Chapter2.md'
+        text = markdown_to_outline(doc.read_text())
+        self.assertNumLines(text, 21, 22)
+        text = extract_outline(text, '2.3')
+        self.assertNumLines(text, 6, 6)
+        # print(text)
+
+    def test_chatter(self):
+        output = 'GhostWriter/AI/Pub/Outline.md'  
+        context = 'GhostWriter/AI/Pub/Persona.md'  
+        content = 'GhostWriter/AI/Pub/TOC.md'  
+        task = 'GhostWriter/AI/Pub/Outline.ai' 
+        task = None   # Disable the AI API call
+        print(do_gpt_task([output, task, context, content]))
+
